9b8254ad26badb882a7ff8efa6c9db1ec3ba3e81
poswalsameer <poswalsameer2003@gmail.com>
2024-04-11 22:53:31 +0530

    added custom cursor, need to change the hover states now

--- portfolio/src/app/components/Hero.tsx
+++ portfolio/src/app/components/Hero.tsx
@@ -26,7 +26,7 @@ function Hero() {
         <div className=' flex flex-col justify-center items-center ' >
 
             <h1 className='text-8xl text-[#faf5f0] font-extrabold my-6'>
-                <span className='text-[#faf5f0]'>SAMEER</span> POSWAL
+                <b>S</b><b>A</b><b>M</b><b>E</b><b>E</b><b>R</b> <b>P</b><b>O</b><b>S</b><b>W</b><b>A</b><b>L</b>
             </h1>
 
             <div className='flex flex-row text-5xl text-[#c3a995] font-semibold my-3 ' >
@@ -35,16 +35,16 @@ function Hero() {
             </div>
 
             <div className='flex flex-row mt-20' >
-                <a href="https://github.com/poswalsameer" target="_blank"><img src="github.png" alt="" className='bg-[#f0e2d3] p-2 rounded-xl h-12 w-12 border-2 border-[#f0e2d3] transition-all ease-in delay-75 hover:scale-110'/></a> 
+                <a href="https://github.com/poswalsameer" target="_blank"><img src="github.png" alt="" className='bg-[#f0e2d3] p-2 rounded-xl h-12 w-12 border-2 border-[#f0e2d3] transition-all ease-in delay-75 hover:scale-110 hover:cursor-none'/></a> 
 
-                <a href="https://leetcode.com/poswalsameer/" target="_blank"><img src="leetcode.png" alt="" className='bg-[#f0e2d3] p-2 rounded-xl h-12 w-12 border-2 border-[#f0e2d3] mx-8 transition-all ease-in hover:scale-110'/></a>
+                <a href="https://leetcode.com/poswalsameer/" target="_blank"><img src="leetcode.png" alt="" className='bg-[#f0e2d3] p-2 rounded-xl h-12 w-12 border-2 border-[#f0e2d3] mx-8 transition-all ease-in hover:scale-110 hover:cursor-none'/></a>
 
-                <a href="https://twitter.com/sameerposwal03" target="_blank"><img src="twitter.png" alt="" className='bg-[#f0e2d3] p-2 rounded-xl h-12 w-12 border-2 border-[#f0e2d3] transition-all ease-in hover:scale-110'/></a>
+                <a href="https://twitter.com/sameerposwal03" target="_blank"><img src="twitter.png" alt="" className='bg-[#f0e2d3] p-2 rounded-xl h-12 w-12 border-2 border-[#f0e2d3] transition-all ease-in hover:scale-110 hover:cursor-none'/></a>
 
-                <a href="https://www.linkedin.com/in/sameerposwal/" target="_blank"><img src="linkedin.png" alt="" className='bg-[#f0e2d3] p-2 rounded-xl h-12 w-12 border-2 border-[#f0e2d3] mx-8 transition-all ease-in hover:scale-110'/></a>
+                <a href="https://www.linkedin.com/in/sameerposwal/" target="_blank"><img src="linkedin.png" alt="" className='bg-[#f0e2d3] p-2 rounded-xl h-12 w-12 border-2 border-[#f0e2d3] mx-8 transition-all ease-in hover:scale-110 hover:cursor-none'/></a>
             </div>
 
-            <button className='h-12 w-44 font-bold text-sm text-black bg-[#c3a995] rounded-xl -ml-10 my-12 transition-all ease-in hover:bg-[#f0e2d3] hover:text-black hover:border-2 hover:border-[#fefae0] '>
+            <button className='h-12 w-44 font-bold text-sm text-black bg-[#c3a995] rounded-xl -ml-10 my-12 transition-all ease-in hover:bg-[#f0e2d3] hover:text-black hover:border-2 hover:border-[#fefae0] hover:cursor-none '>
                 Download Resume
             </button>
 
--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useEffect, useState, useRef } from "react";
 import Contact from "./components/Contact";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
@@ -8,12 +9,140 @@ import Skills from "./components/Skills";
 
 
 export default function Home() {
+
+  // const circleElement = document.querySelector('.circle');
+
+  // const mouse = { x: 0, y: 0 }; 
+  // const previousMouse = { x: 0, y: 0 } 
+  // const circle = { x: 0, y: 0 }; 
+
+  
+  // let currentScale = 0; 
+  // let currentAngle = 0; 
+
+  
+  // window.addEventListener('mousemove', (e) => {
+  //   mouse.x = e.x;
+  //   mouse.y = e.y;
+  // });
+
+  
+  // const speed = 0.17;
+
+  
+  // const tick = () => {
+    
+  //   circle.x += (mouse.x - circle.x) * speed;
+  //   circle.y += (mouse.y - circle.y) * speed;
+    
+  //   const translateTransform = `translate(${circle.x}px, ${circle.y}px)`;
+
+    
+  //   const deltaMouseX = mouse.x - previousMouse.x;
+  //   const deltaMouseY = mouse.y - previousMouse.y;
+    
+  //   previousMouse.x = mouse.x;
+  //   previousMouse.y = mouse.y;
+    
+  //   const mouseVelocity = Math.min(Math.sqrt(deltaMouseX**2 + deltaMouseY**2) * 4, 150); 
+    
+  //   const scaleValue = (mouseVelocity / 150) * 0.5;
+    
+  //   currentScale += (scaleValue - currentScale) * speed;
+    
+  //   const scaleTransform = `scale(${1 + currentScale}, ${1 - currentScale})`;
+
+    
+  //   const angle = Math.atan2(deltaMouseY, deltaMouseX) * 180 / Math.PI;
+    
+  //   if (mouseVelocity > 20) {
+  //     currentAngle = angle;
+  //   }
+    
+  //   const rotateTransform = `rotate(${currentAngle}deg)`;
+
+  //   circleElement.style.transform = `${translateTransform} ${rotateTransform} ${scaleTransform}`;
+
+    
+  //   window.requestAnimationFrame(tick);
+  // }
+
+  // tick();
+
+  const circleRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const circleElement = circleRef.current;
+
+    const mouse = { x: 0, y: 0 };
+    const previousMouse = { x: 0, y: 0 };
+    const circle = { x: 0, y: 0 };
+
+    let currentScale = 0;
+    let currentAngle = 0;
+
+    const handleMouseMove = (e: MouseEvent) => {
+      mouse.x = e.clientX;
+      mouse.y = e.clientY;
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    const speed = 0.17;
+
+    const tick = () => {
+      circle.x += (mouse.x - circle.x) * speed;
+      circle.y += (mouse.y - circle.y) * speed;
+
+      const translateTransform = `translate(${circle.x}px, ${circle.y}px)`;
+
+      const deltaMouseX = mouse.x - previousMouse.x;
+      const deltaMouseY = mouse.y - previousMouse.y;
+
+      previousMouse.x = mouse.x;
+      previousMouse.y = mouse.y;
+
+      const mouseVelocity = Math.min(
+        Math.sqrt(deltaMouseX ** 2 + deltaMouseY ** 2) * 4,
+        150
+      );
+
+      const scaleValue = (mouseVelocity / 150) * 0.5;
+
+      currentScale += (scaleValue - currentScale) * speed;
+
+      const scaleTransform = `scale(${1 + currentScale}, ${1 - currentScale})`;
+
+      const angle = (Math.atan2(deltaMouseY, deltaMouseX) * 180) / Math.PI;
+
+      if (mouseVelocity > 20) {
+        currentAngle = angle;
+      }
+
+      const rotateTransform = `rotate(${currentAngle}deg)`;
+
+      if (circleElement) {
+        circleElement.style.transform = `${translateTransform} ${rotateTransform} ${scaleTransform}`;
+      }
+
+      window.requestAnimationFrame(tick);
+    };
+
+    tick();
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+
   return (
-    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
       
     <>
         
-        <div className="h-screen w-full font-poppins">
+        <div className="h-screen w-full font-poppins ">
+
+        <div id="circle" ref={circleRef}></div>
 
         <Header />
         <Hero />
