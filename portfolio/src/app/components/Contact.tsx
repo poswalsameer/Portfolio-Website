75bf7081f6774ce147e9bf17f1ac276d4b1a9541
poswalsameer <poswalsameer2003@gmail.com>
2024-09-16 23:55:26 +0530

    code cleanup

--- portfolio/src/app/components/Contact.tsx
+++ portfolio/src/app/components/Contact.tsx
@@ -5,44 +5,31 @@ function Contact() {
 
     <>
 
-    <h1 className='text-2xl font-bold mx-4 mt-0 mb-10 text-[#fefae0]
-    
+    <h1 className='
+    text-2xl font-bold mx-4 mt-0 mb-10 text-[#fefae0]
     sm:text-4xl sm:font-bold sm:mx-6 sm:mt-0 sm:mb-10 sm:text-[#fefae0]
-    
     md:text-4xl md:font-bold md:mx-8 md:mt-0 md:mb-5 md:text-[#fefae0]
-    
     lg:text-5xl lg:font-bold lg:mx-10 lg:mt-0 lg:mb-5 lg:text-[#fefae0]
-    
     xl:text-5xl xl:font-bold xl:mx-10 xl:mt-0 xl:mb-5 xl:text-[#fefae0]
-    
     2xl:text-5xl 2xl:font-bold 2xl:mx-12 2xl:mt-0 2xl:mb-5 2xl:text-[#fefae0]' id='contact'>
         &lt;CONTACT /&gt;
     </h1>
 
-    <div className='mt-0 py-10 mx-6 bg-[#322c24] rounded-xl p-5 text-white text-lg flex flex-col justify-center items-center    
-    
+    <div className='
+    mt-0 py-10 mx-6 bg-[#322c24] rounded-xl p-5 text-white text-lg flex flex-col justify-center items-center    
     sm:mt-0 sm:py-10 sm:mx-14 sm:bg-[#322c24] sm:rounded-xl sm:p-5 sm:text-white sm:text-lg sm:flex sm:flex-col sm:justify-center sm:items-center
-    
     md:mt-14 md:py-10 md:mx-20 md:bg-[#322c24] md:rounded-xl md:p-5 md:text-white md:text-lg md:flex md:flex-col md:justify-center md:items-center
-    
     lg:mt-14 lg:py-10 lg:mx-20 lg:bg-[#322c24] lg:rounded-xl lg:p-5 lg:text-white lg:text-lg lg:flex lg:flex-col lg:justify-center lg:items-center
-    
     xl:mt-14 xl:py-10 xl:mx-20 xl:bg-[#322c24] xl:rounded-xl xl:p-5 xl:text-white xl:text-lg xl:flex xl:flex-col xl:justify-center xl:items-center
-    
     2xl:mt-14 2xl:py-10 2xl:mx-20 2xl:bg-[#322c24] 2xl:rounded-xl 2xl:p-5 2xl:text-white 2xl:text-lg 2xl:flex 2xl:flex-col 2xl:justify-center 2xl:items-center' >
 
-        <div className='mx-5 text-center
-
+        <div className='
+        mx-5 text-center
         my-5 flex flex-col justify-center items-center
-        
         sm:my-5 sm:flex sm:flex-col sm:justify-center sm:items-center
-
         md:my-5 md:flex md:flex-col md:justify-center md:items-center 
-
         lg:my-5 lg:flex lg:flex-col lg:justify-center lg:items-center 
-        
         xl:my-5 xl:flex xl:flex-col xl:justify-center xl:items-center 
-        
         2xl:my-5 2xl:flex 2xl:flex-col 2xl:justify-center 2xl:items-center'>
             <p className='text-xs sm:text-base'>
                 YOU CAN ALWAYS CONNECT WITH ME TO DISCUSS ABOUT TECH, ENGINEERING, GAMES, EMULATION!
@@ -50,19 +37,17 @@ function Contact() {
 
         </div>
 
-        <div className='my-5 flex flex-col w-full justify-center items-center
-        
+        <div className='
+        my-5 flex flex-col w-full justify-center items-center
         sm:my-5 sm:flex sm:flex-col sm:w-full sm:justify-center sm:items-center
-        
         md:my-5 md:flex md:flex-row md:w-full md:justify-center md:items-center
-        
         lg:my-5 lg:flex lg:flex-row lg:w-full lg:justify-center lg:items-center
-        
         xl:my-5 xl:flex xl:flex-row xl:w-full xl:justify-center xl:items-center 
-        
         2xl:my-5 2xl:flex 2xl:flex-row 2xl:w-full 2xl:justify-center 2xl:items-center '>
             
-            <div className='bg-[#faf5f0] text-[#1e1b17] text-xs flex flex-row my-2 p-2 justify-center items-center font-bold w-[50%] rounded-lg transition-all ease-in hover:scale-105
+            <div className='
+            
+            bg-[#faf5f0] text-[#1e1b17] text-xs flex flex-row my-2 p-2 justify-center items-center font-bold w-[50%] rounded-lg transition-all ease-in hover:scale-105
             
             sm:bg-[#faf5f0] sm:text-[#1e1b17] sm:text-sm sm:flex sm:flex-row sm:mr-0 sm:p-2 sm:justify-center sm:items-center sm:font-bold sm:w-[40%] sm:rounded-lg sm:transition-all sm:ease-in sm:hover:scale-105
             
