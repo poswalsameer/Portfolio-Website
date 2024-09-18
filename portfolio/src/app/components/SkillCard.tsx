5f3a1ea7c6c02c2979bdf036e023ea942837c3ad
poswalsameer <poswalsameer2003@gmail.com>
2024-09-19 00:23:55 +0530

    added new styling to skills section

--- portfolio/src/app/components/SkillCard.tsx
+++ portfolio/src/app/components/SkillCard.tsx
@@ -3,20 +3,20 @@ import React from 'react'
 function SkillCard(props: any) {
   return (
     <div className='
-    h-8 w-9 my-2 bg-[#faf5f0] bg-opacity-40 flex justify-center items-center rounded-md transition-all ease-in hover:scale-110 hover:bg-opacity-90
-    sm:h-8 sm:w-12 sm:my-2 sm:bg-[#faf5f0] sm:bg-opacity-40 sm:flex sm:justify-center sm:items-center sm:rounded-lg sm:transition-all sm:ease-in sm:hover:scale-110 sm:hover:bg-opacity-90
-    md:h-10 md:w-14 md:my-2 md:bg-[#faf5f0] md:bg-opacity-40 md:flex md:justify-center md:items-center md:rounded-lg md:transition-all md:ease-in md:hover:scale-110 md:hover:bg-opacity-90
-    lg:h-14 lg:w-20 lg:my-2 lg:bg-[#faf5f0] lg:bg-opacity-40 lg:flex lg:justify-center lg:items-center lg:rounded-lg lg:transition-all lg:ease-in lg:hover:scale-110 lg:hover:bg-opacity-90
+    h-7 w-8 my-1 bg-[#faf5f0] bg-opacity-40 flex justify-center items-center rounded-[0.25rem] transition-all ease-in hover:scale-110 hover:bg-opacity-90
+    sm:h-8 sm:w-12 sm:my-1 sm:bg-[#faf5f0] sm:bg-opacity-40 sm:flex sm:justify-center sm:items-center sm:rounded-[0.25rem] sm:transition-all sm:ease-in sm:hover:scale-110 sm:hover:bg-opacity-90
+    md:h-9 md:w-12 md:my-1 md:bg-[#faf5f0] md:bg-opacity-40 md:flex md:justify-center md:items-center md:rounded-[0.25rem] md:transition-all md:ease-in md:hover:scale-110 md:hover:bg-opacity-90
+    lg:h-12 lg:w-16 lg:my-2 lg:bg-[#faf5f0] lg:bg-opacity-40 lg:flex lg:justify-center lg:items-center lg:rounded-md lg:transition-all lg:ease-in lg:hover:scale-110 lg:hover:bg-opacity-90
     xl:h-14 xl:w-20 xl:my-2 xl:bg-[#faf5f0] xl:bg-opacity-40 xl:flex xl:justify-center xl:items-center xl:rounded-lg xl:transition-all xl:ease-in xl:hover:scale-110 xl:hover:bg-opacity-90
     2xl:h-14 2xl:w-20 2xl:my-2 2xl:bg-[#faf5f0] 2xl:bg-opacity-40 2xl:flex 2xl:justify-center 2xl:items-center 2xl:rounded-lg 2xl:transition-all 2xl:ease-in 2xl:hover:scale-110 2xl:hover:bg-opacity-90 ' >
       <img src={props.image} alt="" 
       className='
       h-4 w-5
-      sm:h-6 sm:w-7
-      md:h-6 md:w-7
-      lg:h-6 lg:w-7
+      sm:h-4 sm:w-5
+      md:h-4 md:w-5
+      lg:h-5 lg:w-6
       xl:h-6 xl:w-7 
-      2xl:h-7 2xl:w-8'/>
+      2xl:h-6 2xl:w-7'/>
     </div>
   )
 }
--- portfolio/src/app/components/Skills.tsx
+++ portfolio/src/app/components/Skills.tsx
@@ -15,172 +15,169 @@ function Skills() {
             &lt;TECHNOLOGIES I KNOW /&gt;
         </h1>
 
-        <div className='
-        grid place-content-center place-items-center grid-cols-1 gap-y-5
-        sm:grid sm:grid-cols-1 sm:gap-y-5
-        md:grid md:grid-cols-2 md:gap-y-5
-        lg:grid lg:grid-cols-2 lg:gap-y-5
-        xl:grid xl:grid-cols-2 xl:gap-y-5
-        2xl:grid 2xl:grid-cols-2 2xl:gap-y-5' >
+        <div className='h-full w-full flex justify-center items-center'>
 
-            {/* FRONTEND WALA CARD */}
             <div className='
-            flex flex-col justify-center items-center h-[6.5rem] w-[95%] border-2 border-[#faf5f0] border-opacity-40 mx-14 rounded-xl
-            sm:flex sm:flex-col sm:justify-center sm:items-center sm:h-32 sm:w-[93%] sm:border-2 sm:border-[#faf5f0] sm:border-opacity-40 sm:mx-10 sm:rounded-xl
-            md:flex md:flex-col md:justify-center md:items-center md:h-48 md:w-[90%] md:border-2 md:border-[#faf5f0] md:border-opacity-40 md:mx-10 md:rounded-xl
-            lg:flex lg:flex-col lg:justify-center lg:items-center lg:h-56 lg:w-[90%] lg:border-2 lg:border-[#faf5f0] lg:border-opacity-40 lg:mx-10 lg:rounded-xl
-            xl:flex xl:flex-col xl:justify-center xl:items-center xl:h-56 xl:w-[90%] xl:border-2 xl:border-[#faf5f0] xl:border-opacity-40 xl:mx-10 xl:rounded-xl
-            2xl:flex 2xl:flex-col 2xl:justify-center 2xl:items-center 2xl:h-56 2xl:w-[90%] 2xl:border-2 2xl:border-[#faf5f0] 2xl:border-opacity-40 2xl:mx-10 2xl:rounded-xl'>
+            w-[100%] grid place-content-center place-items-center grid-cols-1 gap-y-5
+            sm:w-[90%] sm:grid sm:grid-cols-1 sm:gap-y-5
+            md:w-[90%] md:grid md:grid-cols-2 md:gap-y-5
+            lg:w-[90%] lg:grid lg:grid-cols-2 lg:gap-y-5
+            xl:w-[90%] xl:grid xl:grid-cols-2 xl:gap-y-5
+            2xl:w-[85%] 2xl:grid 2xl:grid-cols-2 2xl:gap-y-5' >
 
-                <p className='
-                text-base text-[#faf5f0] font-semibold my-1
-                sm:text-base sm:text-[#faf5f0] sm:font-semibold sm:my-2
-                md:text-base md:text-[#faf5f0] md:font-semibold md:my-2
-                lg:text-base lg:text-[#faf5f0] lg:font-semibold lg:my-2
-                xl:text-base xl:text-[#faf5f0] xl:font-semibold xl:my-2
-                2xl:text-base 2xl:text-[#faf5f0] 2xl:font-semibold 2xl:my-2'>
-                    Frontend Technologies
-                </p>
-
+                {/* FRONTEND WALA CARD */}
                 <div className='
-                grid grid-cols-8 gap-x-1
-                sm:grid sm:grid-cols-8 sm:gap-x-3
-                md:grid md:grid-cols-4 md:gap-x-3
-                lg:grid lg:grid-cols-4 lg:gap-x-3
-                xl:grid xl:grid-cols-4 xl:gap-x-3
-                2xl:grid 2xl:grid-cols-4 2xl:gap-x-3'>
+                flex flex-col justify-center items-center h-[8.5rem] w-[95%] border-2 border-[#faf5f0] border-opacity-40 mx-14 rounded-md
+                sm:flex sm:flex-col sm:justify-center sm:items-center sm:h-40 sm:w-[93%] sm:border-2 sm:border-[#faf5f0] sm:border-opacity-40 sm:mx-10 sm:rounded-lg
+                md:flex md:flex-col md:justify-center md:items-center md:h-56 md:w-[90%] md:border-2 md:border-[#faf5f0] md:border-opacity-40 md:mx-10 md:rounded-xl
+                lg:flex lg:flex-col lg:justify-center lg:items-center lg:h-72 lg:w-[90%] lg:border-2 lg:border-[#faf5f0] lg:border-opacity-40 lg:mx-10 lg:rounded-xl
+                xl:flex xl:flex-col xl:justify-center xl:items-center xl:h-72 xl:w-[90%] xl:border-2 xl:border-[#faf5f0] xl:border-opacity-40 xl:mx-10 xl:rounded-xl
+                2xl:flex 2xl:flex-col 2xl:justify-center 2xl:items-center 2xl:h-72 2xl:w-[90%] 2xl:border-2 2xl:border-[#faf5f0] 2xl:border-opacity-40 2xl:mx-10 2xl:rounded-xl'>
 
-                    <SkillCard image='html.png' />
-                    <SkillCard image='css.png' />
-                    <SkillCard image='javascript.png' />
-                    <SkillCard image='react.png' />
-                    <SkillCard image='next.png' />
-                    <SkillCard image='tailwind.png' />
-                    <SkillCard image='bootstrap.png' />
-                    <SkillCard image='npm.png' />
-                    <SkillCard image='shadcn.png' />
+                    <p className='
+                    text-base text-[#faf5f0] font-semibold my-1
+                    sm:text-base sm:text-[#faf5f0] sm:font-semibold sm:my-2
+                    md:text-base md:text-[#faf5f0] md:font-semibold md:my-2
+                    lg:text-base lg:text-[#faf5f0] lg:font-semibold lg:my-2
+                    xl:text-base xl:text-[#faf5f0] xl:font-semibold xl:my-2
+                    2xl:text-base 2xl:text-[#faf5f0] 2xl:font-semibold 2xl:my-2'>
+                        Frontend Technologies
+                    </p>
+
+                    <div className='
+                    grid grid-cols-8 gap-x-1
+                    sm:grid sm:grid-cols-6 sm:gap-x-3
+                    md:grid md:grid-cols-4 md:gap-x-3
+                    lg:grid lg:grid-cols-4 lg:gap-x-3
+                    xl:grid xl:grid-cols-4 xl:gap-x-3
+                    2xl:grid 2xl:grid-cols-4 2xl:gap-x-3'>
+
+                        <SkillCard image='html.png' />
+                        <SkillCard image='css.png' />
+                        <SkillCard image='javascript.png' />
+                        <SkillCard image='typescript.png' />
+                        <SkillCard image='react.png' />
+                        <SkillCard image='next.png' />
+                        <SkillCard image='redux.svg' />
+                        <SkillCard image='tailwind.png' />
+                        <SkillCard image='bootstrap.png' />
+                        <SkillCard image='npm.png' />
+
+                    </div>
 
                 </div>
 
-            </div>
-
-            {/* BACKEND TECH WALA SECTION */}
-            <div className='
-            flex flex-col justify-center items-center h-[6.5rem] w-[95%] border-2 border-[#faf5f0] border-opacity-40 mx-14 rounded-xl
-            sm:flex sm:flex-col sm:justify-center sm:items-center sm:h-32 sm:w-[93%] sm:border-2 sm:border-[#faf5f0] sm:border-opacity-40 sm:mx-10 sm:rounded-xl
-            md:flex md:flex-col md:justify-center md:items-center md:h-48 md:w-[90%] md:border-2 md:border-[#faf5f0] md:border-opacity-40 md:mx-10 md:rounded-xl
-            lg:flex lg:flex-col lg:justify-center lg:items-center lg:h-56 lg:w-[90%] lg:border-2 lg:border-[#faf5f0] lg:border-opacity-40 lg:mx-10 lg:rounded-xl
-            xl:flex xl:flex-col xl:justify-center xl:items-center xl:h-56 xl:w-[90%] xl:border-2 xl:border-[#faf5f0] xl:border-opacity-40 xl:mx-10 xl:rounded-xl
-            2xl:flex 2xl:flex-col 2xl:justify-center 2xl:items-center 2xl:h-56 2xl:w-[90%] 2xl:border-2 2xl:border-[#faf5f0] 2xl:border-opacity-40 2xl:mx-10 2xl:rounded-xl'>
-
-                <p className='
-                text-base text-[#faf5f0] font-semibold my-1
-                sm:text-base sm:text-[#faf5f0] sm:font-semibold sm:my-2
-                md:text-base md:text-[#faf5f0] md:font-semibold md:my-2
-                lg:text-base lg:text-[#faf5f0] lg:font-semibold lg:my-2
-                xl:text-[#faf5f0] xl:font-semibold xl:my-2
-                2xl:text-[#faf5f0] 2xl:font-semibold 2xl:my-2'>
-                    Backend Technologies
-                </p>
-
+                {/* BACKEND TECH WALA SECTION */}
                 <div className='
-                grid grid-cols-6 gap-x-2
-                sm:grid sm:grid-cols-6 sm:gap-x-3
-                md:grid md:grid-cols-4 md:gap-x-3
-                lg:grid lg:grid-cols-4 lg:gap-x-3
-                xl:grid xl:grid-cols-4 xl:gap-x-3
-                2xl:grid 2xl:grid-cols-4 2xl:gap-x-3 '>
+                flex flex-col justify-center items-center h-[8.5rem] w-[95%] border-2 border-[#faf5f0] border-opacity-40 mx-14 rounded-md
+                sm:flex sm:flex-col sm:justify-center sm:items-center sm:h-40 sm:w-[93%] sm:border-2 sm:border-[#faf5f0] sm:border-opacity-40 sm:mx-10 sm:rounded-lg
+                md:flex md:flex-col md:justify-center md:items-center md:h-56 md:w-[90%] md:border-2 md:border-[#faf5f0] md:border-opacity-40 md:mx-10 md:rounded-xl
+                lg:flex lg:flex-col lg:justify-center lg:items-center lg:h-72 lg:w-[90%] lg:border-2 lg:border-[#faf5f0] lg:border-opacity-40 lg:mx-10 lg:rounded-xl
+                xl:flex xl:flex-col xl:justify-center xl:items-center xl:h-72 xl:w-[90%] xl:border-2 xl:border-[#faf5f0] xl:border-opacity-40 xl:mx-10 xl:rounded-xl
+                2xl:flex 2xl:flex-col 2xl:justify-center 2xl:items-center 2xl:h-72 2xl:w-[90%] 2xl:border-2 2xl:border-[#faf5f0] 2xl:border-opacity-40 2xl:mx-10 2xl:rounded-xl'>
 
-                    <SkillCard image='javascript.png' />
-                    <SkillCard image='nodejs.png' />
-                    <SkillCard image='express.png' />
-                    <SkillCard image='npm.png' />
-                    <SkillCard image='mongo.png' />
-                    <SkillCard image='sql.png' />
-                    <SkillCard image='docker.png' />
+                    <p className='
+                    text-base text-[#faf5f0] font-semibold my-1
+                    sm:text-base sm:text-[#faf5f0] sm:font-semibold sm:my-2
+                    md:text-base md:text-[#faf5f0] md:font-semibold md:my-2
+                    lg:text-base lg:text-[#faf5f0] lg:font-semibold lg:my-2
+                    xl:text-[#faf5f0] xl:font-semibold xl:my-2
+                    2xl:text-[#faf5f0] 2xl:font-semibold 2xl:my-2'>
+                        Backend Technologies
+                    </p>
+
+                    <div className='
+                    grid grid-cols-6 gap-x-1
+                    sm:grid sm:grid-cols-6 sm:gap-x-3
+                    md:grid md:grid-cols-4 md:gap-x-3
+                    lg:grid lg:grid-cols-4 lg:gap-x-3
+                    xl:grid xl:grid-cols-4 xl:gap-x-3
+                    2xl:grid 2xl:grid-cols-4 2xl:gap-x-3 '>
+
+                        <SkillCard image='javascript.png' />
+                        <SkillCard image='nodejs.png' />
+                        <SkillCard image='express.png' />
+                        <SkillCard image='npm.png' />
+                        <SkillCard image='mongo.png' />
+                        <SkillCard image='sql.png' />
+                        <SkillCard image='appwrite.png' />
+
+                    </div>
 
                 </div>
 
-            </div>
 
-
-            {/* DATABASES */}
-            <div className='
-            flex flex-col justify-center items-center h-[6.5rem] w-[95%] border-2 border-[#faf5f0] border-opacity-40 mx-14 rounded-xl
-            sm:flex sm:flex-col sm:justify-center sm:items-center sm:h-32 sm:w-[93%] sm:border-2 sm:border-[#faf5f0] sm:border-opacity-40 sm:mx-10 sm:rounded-xl
-            md:flex md:flex-col md:justify-center md:items-center md:h-48 md:w-[90%] md:border-2 md:border-[#faf5f0] md:border-opacity-40 md:mx-10 md:rounded-xl
-            lg:flex lg:flex-col lg:justify-center lg:items-center lg:h-56 lg:w-[90%] lg:border-2 lg:border-[#faf5f0] lg:border-opacity-40 lg:mx-10 lg:rounded-xl
-            xl:flex xl:flex-col xl:justify-center xl:items-center xl:h-56 xl:w-[90%] xl:border-2 xl:border-[#faf5f0] xl:border-opacity-40 xl:mx-10 xl:rounded-xl
-            2xl:flex 2xl:flex-col 2xl:justify-center 2xl:items-center 2xl:h-56 2xl:w-[90%] 2xl:border-2 2xl:border-[#faf5f0] 2xl:border-opacity-40 2xl:mx-10 2xl:rounded-xl'>
-
-                <p className='
-                text-base text-[#faf5f0] font-semibold my-1
-                sm:text-base sm:text-[#faf5f0] sm:font-semibold sm:my-2
-                md:text-base md:text-[#faf5f0] md:font-semibold md:my-2
-                lg:text-base lg:text-[#faf5f0] lg:font-semibold lg:my-2
-                xl:text-[#faf5f0] xl:font-semibold xl:my-2
-                2xl:text-[#faf5f0] 2xl:font-semibold 2xl:my-2'>
-                    Programming Languages
-                </p>
-
+                {/* DATABASES */}
                 <div className='
-                grid grid-cols-4 gap-x-2
-                sm:grid sm:grid-cols-4 sm:gap-x-3
-                md:grid md:grid-cols-4 md:gap-x-3
-                lg:grid lg:grid-cols-4 lg:gap-x-3
-                xl:grid xl:grid-cols-4 xl:gap-x-3
-                2xl:grid 2xl:grid-cols-4 2xl:gap-x-3 '>
+                flex flex-col justify-center items-center h-[8.5rem] w-[95%] border-2 border-[#faf5f0] border-opacity-40 mx-14 rounded-md
+                sm:flex sm:flex-col sm:justify-center sm:items-center sm:h-40 sm:w-[93%] sm:border-2 sm:border-[#faf5f0] sm:border-opacity-40 sm:mx-10 sm:rounded-lg
+                md:flex md:flex-col md:justify-center md:items-center md:h-56 md:w-[90%] md:border-2 md:border-[#faf5f0] md:border-opacity-40 md:mx-10 md:rounded-xl
+                lg:flex lg:flex-col lg:justify-center lg:items-center lg:h-72 lg:w-[90%] lg:border-2 lg:border-[#faf5f0] lg:border-opacity-40 lg:mx-10 lg:rounded-xl
+                xl:flex xl:flex-col xl:justify-center xl:items-center xl:h-72 xl:w-[90%] xl:border-2 xl:border-[#faf5f0] xl:border-opacity-40 xl:mx-10 xl:rounded-xl
+                2xl:flex 2xl:flex-col 2xl:justify-center 2xl:items-center 2xl:h-72 2xl:w-[90%] 2xl:border-2 2xl:border-[#faf5f0] 2xl:border-opacity-40 2xl:mx-10 2xl:rounded-xl'>
 
-                    <SkillCard image='cpp.png' />
-                    <SkillCard image='javascript.png' />
-                    <SkillCard image='csharp.png' />
-                    <SkillCard image='python.png' /> 
-                    <SkillCard image='golang.png' />                    
+                    <p className='
+                    text-base text-[#faf5f0] font-semibold my-1
+                    sm:text-base sm:text-[#faf5f0] sm:font-semibold sm:my-2
+                    md:text-base md:text-[#faf5f0] md:font-semibold md:my-2
+                    lg:text-base lg:text-[#faf5f0] lg:font-semibold lg:my-2
+                    xl:text-[#faf5f0] xl:font-semibold xl:my-2
+                    2xl:text-[#faf5f0] 2xl:font-semibold 2xl:my-2'>
+                        Programming Languages
+                    </p>
+
+                    <div className='
+                    grid grid-cols-6 gap-x-1
+                    sm:grid sm:grid-cols-6 sm:gap-x-3
+                    md:grid md:grid-cols-4 md:gap-x-3
+                    lg:grid lg:grid-cols-4 lg:gap-x-3
+                    xl:grid xl:grid-cols-4 xl:gap-x-3
+                    2xl:grid 2xl:grid-cols-4 2xl:gap-x-3 '>
+
+                        <SkillCard image='cpp.png' />
+                        <SkillCard image='javascript.png' />
+                        <SkillCard image='typescript.png' />
+                        <SkillCard image='csharp.png' />
+                        <SkillCard image='python.png' /> 
+                        <SkillCard image='golang.png' />                    
+
+                    </div>
 
                 </div>
 
-            </div>
+                {/* GAME DEVELOPMENT WALA SECTION */}
 
-            {/* GAME DEVELOPMENT WALA SECTION */}
+                <div className='
+                flex flex-col justify-center items-center h-[8.5rem] w-[95%] border-2 border-[#faf5f0] border-opacity-40 mx-14 rounded-md
+                sm:flex sm:flex-col sm:justify-center sm:items-center sm:h-40 sm:w-[93%] sm:border-2 sm:border-[#faf5f0] sm:border-opacity-40 sm:mx-10 sm:rounded-lg
+                md:flex md:flex-col md:justify-center md:items-center md:h-56 md:w-[90%] md:border-2 md:border-[#faf5f0] md:border-opacity-40 md:mx-10 md:rounded-xl
+                lg:flex lg:flex-col lg:justify-center lg:items-center lg:h-72 lg:w-[90%] lg:border-2 lg:border-[#faf5f0] lg:border-opacity-40 lg:mx-10 lg:rounded-xl
+                xl:flex xl:flex-col xl:justify-center xl:items-center xl:h-72 xl:w-[90%] xl:border-2 xl:border-[#faf5f0] xl:border-opacity-40 xl:mx-10 xl:rounded-xl
+                2xl:flex 2xl:flex-col 2xl:justify-center 2xl:items-center 2xl:h-72 2xl:w-[90%] 2xl:border-2 2xl:border-[#faf5f0] 2xl:border-opacity-40 2xl:mx-10 2xl:rounded-xl'>
 
-            <div className='flex flex-col justify-center items-center h-[6.5rem] w-[95%] border-2 border-[#faf5f0] border-opacity-40 mx-14 rounded-xl
-            
-            sm:flex sm:flex-col sm:justify-center sm:items-center sm:h-32 sm:w-[93%] sm:border-2 sm:border-[#faf5f0] sm:border-opacity-40 sm:mx-10 sm:rounded-xl
-            
-            md:flex md:flex-col md:justify-center md:items-center md:h-48 md:w-[90%] md:border-2 md:border-[#faf5f0] md:border-opacity-40 md:mx-10 md:rounded-xl
-            
-            lg:flex lg:flex-col lg:justify-center lg:items-center lg:h-56 lg:w-[90%] lg:border-2 lg:border-[#faf5f0] lg:border-opacity-40 lg:mx-10 lg:rounded-xl
-            
-            xl:flex xl:flex-col xl:justify-center xl:items-center xl:h-56 xl:w-[90%] xl:border-2 xl:border-[#faf5f0] xl:border-opacity-40 xl:mx-10 xl:rounded-xl
-            
-            2xl:flex 2xl:flex-col 2xl:justify-center 2xl:items-center 2xl:h-56 2xl:w-[90%] 2xl:border-2 2xl:border-[#faf5f0] 2xl:border-opacity-40 2xl:mx-10 2xl:rounded-xl'>
+                    <p className='
+                    text-base text-[#faf5f0] font-semibold my-1
+                    sm:text-base sm:text-[#faf5f0] sm:font-semibold sm:my-2
+                    md:text-base md:text-[#faf5f0] md:font-semibold md:my-2
+                    lg:text-base lg:text-[#faf5f0] lg:font-semibold lg:my-2
+                    xl:text-[#faf5f0] xl:font-semibold xl:my-2
+                    2xl:text-[#faf5f0] 2xl:font-semibold 2xl:my-2'>
+                        Other tools
+                    </p>
 
-                <p className='text-base text-[#faf5f0] font-semibold my-1
-                
-                sm:text-base sm:text-[#faf5f0] sm:font-semibold sm:my-2
-                
-                md:text-base md:text-[#faf5f0] md:font-semibold md:my-2
-                
-                lg:text-base lg:text-[#faf5f0] lg:font-semibold lg:my-2
-                
-                xl:text-[#faf5f0] xl:font-semibold xl:my-2
-                
-                2xl:text-[#faf5f0] 2xl:font-semibold 2xl:my-2'>Game Development</p>
+                    <div className='
+                        grid grid-cols-3 gap-x-1
+                        sm:grid sm:grid-cols-3 sm:gap-x-3
+                        md:grid md:grid-cols-2 md:gap-x-3
+                        lg:grid lg:grid-cols-2 lg:gap-x-3
+                        xl:grid xl:grid-cols-2 xl:gap-x-3
+                        2xl:grid 2xl:grid-cols-2 2xl:gap-x-3 '>
 
-                <div className='grid grid-cols-2 gap-x-2
-                
-                sm:grid sm:grid-cols-2 sm:gap-x-3
-                
-                md:grid md:grid-cols-2 md:gap-x-3
-                
-                lg:grid lg:grid-cols-2 lg:gap-x-3
-                
-                xl:grid xl:grid-cols-2 xl:gap-x-3
-                
-                2xl:grid 2xl:grid-cols-2 2xl:gap-x-3 '>
+                        <SkillCard image='unity.png' />   
+                        <SkillCard image='vercel-logo.svg' /> 
+                        <SkillCard image='docker.png' />                
 
-                    <SkillCard image='unity.png' />
-                    <SkillCard image='csharp.png' />                    
+                    </div>
 
                 </div>
 
